c77919086a2b25b179eca28b7ba9f741152ea423
meinhardtmr <meinhardtmr@netscape.net>
2023-07-10 18:37:48 -0400

    updated get_json.ipynb createStationInfo.js index.html geo_alt_fuel_stations.json

--- Project-3/static/js/createStationInfo.js
+++ Project-3/static/js/createStationInfo.js
@@ -3,20 +3,35 @@
 function createStationInfo(value){
   if (value == 'ALL'){
     logging ? console.log('createStationInfo: ALL') : null;
-    document.getElementById("stations-metadata").innerHTML = ''; 
+    document.getElementById("stations-metadata").innerHTML = '<pclass="p1">&ltClick map marker for station info&gt</p>'; 
   } else{    
     logging ? console.log('createStationInfo: ', value, stations.find(o => o.properties.id == value).properties.id) : null;
-  
-    let str = `<p class="p1">Station ID: ${stations.find(o => o.properties.id == value).properties.id}</p>
-               <p class="p1">Station Name: ${stations.find(o => o.properties.id == value).properties.station_name}</p>
-               <p class="p1">Street Address: ${stations.find(o => o.properties.id == value).properties.street_address}</p>
-               <p class="p1">City: ${stations.find(o => o.properties.id == value).properties.city}</p>
-               <p class="p1">State: ${stations.find(o => o.properties.id == value).properties.state}</p>
-               <p class="p1">Zip Code: ${stations.find(o => o.properties.id == value).properties.zip}</p>
-               <p class="p1">Phone Number: ${stations.find(o => o.properties.id == value).properties.station_phone}</p>
-               <p class="p1">Fuel Type: ${stations.find(o => o.properties.id == value).properties.fuel_type_desc}</p>`;
     
-    document.getElementById("stations-metadata").innerHTML = str; 
+    const s = stations.find(o => o.properties.id == value);
+        const str = `<p class="p1"><b>Station ID:</b> ${s.properties.id}</p>
+                     <p class="p1"><b>Station Name:</b> ${s.properties.station_name}</p>
+                     <p class="p1"><b>Street Address:</b> ${s.properties.street_address}</p>
+                     <p class="p1"><b>City:</b> ${s.properties.city}</p>
+                     <p class="p1"><b>State:</b> ${s.properties.state}</p>
+                     <p class="p1"><b>Zip Code:</b> ${s.properties.zip}</p>
+                     <p class="p1"><b>Phone Number:</b> ${s.properties.station_phone}</p>
+                     <p class="p1"><b>Fuel Type:</b> ${s.properties.fuel_type_desc}</p>`;
+    let str2 = "";   
+    let fuel_type_code = s.properties.fuel_type_code;
+    if (fuel_type_code === 'ELEC'){
+      str2 = '<p class="p1"><b>EV Connector Types:</b> ' + s.properties.ev_connector_desc + '</p>';
+      str2 = str2 + '<p class="p1"><b>Number of Chargers:</b> ' + (s.properties.ev_dc_fast_num + 
+                                                                   s.properties.ev_level1_evse_num + 
+                                                                   s.properties.ev_level2_evse_num) + '</p>';
+    }
+    else if (fuel_type_code === 'CNG'){
+        str2 = '<p class="p1"><b>Number of Dispensers:</b> ' + (s.properties.cng_dispenser_num) + '</p>';
+      }
+    else{
+        str2 = '<p class="p1"><b>Number of Dispensers:</b> Unknown</p>';
+      }
+    
+    document.getElementById("stations-metadata").innerHTML = str + str2; 
   }
   return;
 }
